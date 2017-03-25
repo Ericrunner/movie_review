944f77a6a875f77a0f97f49df761d021950617a3
liujia <ericrunner123456@yahoo.com>
2017-03-25 16:30:23 +0800

    finish first version

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
+  validates :comment, presence: true
   belongs_to :review
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170323230515) do
+ActiveRecord::Schema.define(version: 20170325072114) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "comment"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20170323230515) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.binary   "image"
   end
 
   create_table "user_favorites", force: :cascade do |t|
